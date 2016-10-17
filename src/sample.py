8d3d2c1ffe4736ec5993ec826e5d7e456bec6afb
hamuhamu <hrdng4922000@yahoo.co.jp>
2016-10-17 23:41:54 +0900

    サンプルコード追加

---
+++ src/sample.py
@@ -0,0 +1,7 @@
+class Sample:
+
+    def __init__(self, arg):
+        self.arg = arg
+
+    def calc()
+        return self.arg * 2
---
+++ test/sample_test.py
@@ -0,0 +1,8 @@
+import pytest
+import src.sample
+
+def test_set_comparison():
+    set1 = set("1308")
+    set2 = set("8035")
+
+    assert set1 == set2
