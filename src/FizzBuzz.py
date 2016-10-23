9267c4ff75781621c331cfc93f1ea275efb4486c
hamuhamu <hrdng4922000@yahoo.co.jp>
2016-10-23 19:02:49 +0900

    __isFizzBuzzメソッドを追加

--- src/FizzBuzz.py
+++ src/FizzBuzz.py
@@ -7,7 +7,7 @@ class FizzBuzz:
         return
 
     def calc(self, arg):
-        if arg % 3 == 0 and arg % 5 == 0:
+        if self.__isFizzBuzz(arg):
             return self.FIZZ_BUZZ
 
         if self.__isFizz(arg):
@@ -19,7 +19,10 @@ class FizzBuzz:
         return arg
 
     def __isFizz(self, arg):
-        return arg % 3 == 0;
+        return arg % 3 == 0
 
     def __isBuzz(self, arg):
-        return arg % 5 == 0;
+        return arg % 5 == 0
+
+    def __isFizzBuzz(self, arg):
+        return self.__isFizz(arg) and self.__isBuzz(arg)
