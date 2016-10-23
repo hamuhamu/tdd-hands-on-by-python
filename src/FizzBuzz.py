6e32b961656073160e5d084f35741e29bcc39051
hamuhamu <hrdng4922000@yahoo.co.jp>
2016-10-23 16:15:27 +0900

    文字列リテラルを定数化した

--- src/FizzBuzz.py
+++ src/FizzBuzz.py
@@ -1,16 +1,19 @@
 class FizzBuzz:
+    FIZZ = 'Fizz'
+    BUZZ = 'Buzz'
+    FIZZ_BUZZ = 'FizzBuzz'
 
     def __init__(self):
         return
 
     def calc(self, arg):
         if arg % 3 == 0 and arg % 5 == 0:
-            return 'FizzBuzz'
+            return self.FIZZ_BUZZ
 
         if arg % 3 == 0:
-            return 'Fizz'
+            return self.FIZZ
 
         if arg % 5 == 0:
-            return 'Buzz'
+            return self.BUZZ
 
         return arg
--- test/fizzbuzz_test.py
+++ test/fizzbuzz_test.py
@@ -3,12 +3,6 @@ import sys
 sys.path.append('src/')
 from FizzBuzz import FizzBuzz
 
-def test_受け取った数値をそのまま返すこと():
-    fizzBuzz = FizzBuzz()
-
-    assert 1 == fizzBuzz.calc(1)
-    assert 2 == fizzBuzz.calc(2)
-
 @pytest.mark.parametrize(('arg', 'expected'), [
     (1, 1),
     (2, 2),
